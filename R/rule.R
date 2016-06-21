416d0ea469826ce1845b710c8dfb48c4247268f1
MJomaba <witek1987@gmail.com>
2016-06-21 14:47:02 -0700

    First simple rule evaluation

--- R/rule.R
+++ R/rule.R
@@ -4,7 +4,36 @@
 #'
 #' @param data The data to validate
 #' @param rule.file The yaml file containing the rules
+#' @export
 rule.validation <- function( data, rules.file )
 {
-  stop("Not implemented yet")
+  rules<-yaml::yaml.load_file(rules.file)
+
+  for(rule in rules)
+  {
+    if(is.rule(rule))
+      apply.rule(data,rule)
+  }
+
+}
+
+apply.rule<-function(data,rule)
+{
+  results <- c()
+  if("relation" %in% names(rule))
+  {
+    if (rule$relation == "greater_than" || rule$relation == ">")
+      results <- data[[rule$field1]] > data[[rule$field2]]
+    if (rule$relation == "lesser_than" || rule$relation == "<")
+      results <- data[[rule$field1]] < data[[rule$field2]]
+  }
+
+  if (!all(results))
+    warning("Rows: ", which(!results)[1], " fail rule: ", rule)
+}
+
+is.rule<-function(x)
+{
+  keywords=c("rule","relation","dependancy")
+  return(any(keywords %in% names(x)))
 }
