45a2a37e8dd9ac7bc2ac3e45153822aebebd8abb
BlackEdder <edder@tkwsping.nl>
2016-06-21 17:27:53 -0700

    Add test for invalid value combination

--- tests/testthat/testRules.R
+++ tests/testthat/testRules.R
@@ -8,3 +8,13 @@ test_that("Rules defined by relation keyword work", {
    expect_true( rule.validation(epi.sim, rules=list(list(
     "field1"="date.of.onset", "field2"="date.of.hospitalisation", "relation"="lte" ) ) ))
 })
+
+test_that("We can exclude relations between fields", {
+
+  data(epi.sim)
+  rules <- list(list("fields"=c("gender","generation"), "values"=list("invalid"=c("f",0))))
+  expect_warning( rule.validation(epi.sim, rules = rules ) )
+
+  rules <- list(list("fields"=c("gender","generation"), "values"=list("invalid"=c("f",10))))
+  expect_true( rule.validation(epi.sim, rules = rules ) )
+})
