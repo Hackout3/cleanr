Hackout3/cleanr

commit 4fc905ca6a2d31ea18a0618da5e726b790b60a5d

Allow two value functions
Fixes #2

We don't have good tests in for anything yet, so @annecori can you test
that things are still working with the set you have?

R/recode.R
@@ -13,10 +13,10 @@ recode <- function(data, rules) {
 
 recode_apply <- function(name, data, rule) {
   if ("from" %in% names(rule)) {
-    x <- data[[rule$from]]
+    x <- unname(data[rule$from])
     fun <- setdiff(names(rule), "from")
   } else {
-    x <- data[[name]]
+    x <- data[name]
     fun <- names(rule)
   }
 
@@ -28,10 +28,10 @@ recode_apply <- function(name, data, rule) {
     }
     ## TODO: using do.call here is really suboptimal when 'x' might be
     ## large.  More background: http://rpubs.com/hadley/do-call2
-    x <- do.call(f, c(list(x), r), quote=TRUE)
+    x <- list(do.call(f, c(x, r), quote=TRUE))
   }
 
-  data[[name]] <- x
+  data[name] <- x
   data
 }
 

tests/testthat/test-recode.R
@@ -1,0 +1,9 @@
+context("recode")
+
+test_that("recode two columns", {
+  rules <- recode_read("two.yml")
+  data <- data.frame(old1=runif(10), old2=runif(10))
+  data2 <- recode(data, rules)
+  expect_true("min" %in% names(data2))
+  expect_equal(data2$min, pmin(data$old1, data$old2))
+})

tests/testthat/two.yml
@@ -1,0 +1,3 @@
+- min:
+    from: [old1, old2]
+    pmin:

commit 1e51df877fd4e31b61d33b87c5dbe644fcf7ed93

Fix bug introduced with 4fc905c

R/recode.R
@@ -16,7 +16,7 @@ recode_apply <- function(name, data, rule) {
     x <- unname(data[rule$from])
     fun <- setdiff(names(rule), "from")
   } else {
-    x <- data[name]
+    x <- unname(data[name])
     fun <- names(rule)
   }
 

tests/testthat/rename.yml
@@ -1,0 +1,3 @@
+- new:
+    - from: old
+    - sqrt

tests/testthat/rename_recipes.yml
@@ -1,0 +1,3 @@
+process:
+  - as.numeric
+  - sqrt

tests/testthat/test-recode.R
@@ -1,5 +1,20 @@
 context("recode")
 
+test_that("rename", {
+  rules <- recode_read("rename.yml")
+  data <- data.frame(old=runif(10))
+  data2 <- recode(data, rules)
+  expect_identical(data2$new, sqrt(data$old))
+})
+
+test_that("recipe", {
+  rules <- recode_read("use_recipe.yml", "rename_recipes.yml")
+  data <- data.frame(old=as.character(runif(10)))
+  data2 <- recode(data, rules)
+  expect_equal(data2$old, sqrt(as.numeric(data$old)))
+  expect_equal(names(data2), names(data))
+})
+
 test_that("recode two columns", {
   rules <- recode_read("two.yml")
   data <- data.frame(old1=runif(10), old2=runif(10))

tests/testthat/use_recipe.yml
@@ -1,0 +1,1 @@
+- old: !recipe process