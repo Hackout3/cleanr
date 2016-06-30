b1f057a9aa79e072fa417fc5df9df221f608fc4b
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-06-30 12:31:03 +0100

    Detect renaming polarity automatically

--- R/rename.R
+++ R/rename.R
@@ -1,13 +1,28 @@
 ## There is _massive_ overlap here with dplyr, but we'll take a
 ## different approach (it's not designed for interactive work but for
 ## bulk processing).
-rename <- function(data, table, reverse_direction=FALSE) {
+rename <- function(data, table, reverse_direction=NULL) {
   if (possible_filename(table) && file.exists(table)) {
     table <- yaml_read(table)
   }
   if (is.list(table)) {
     ## TODO: validation.
     table <- unlist(table)
+  }
+
+  if (length(table) == 0L) {
+    return(data)
+  }
+
+  nms <- names(data)
+  if (is.null(reverse_direction)) {
+    if (all(table %in% nms) && !any(names(table) %in% nms)) {
+      reverse_direction <- TRUE
+    } else if (all(names(table) %in% nms) && !any(table %in% nms)) {
+      reverse_direction <- FALSE
+    } else {
+      stop("Could not guess direction")
+    }
   }
 
   if (reverse_direction) {
@@ -25,17 +40,15 @@ rename <- function(data, table, reverse_direction=FALSE) {
     stop("Duplicated destination columns: ",
          paste(to[dup_to], collapse=", "))
   }
-  if (length(from) > 0L) {
-    i <- match(from, names(data))
-    j <- is.na(i)
+  i <- match(from, nms)
+  j <- is.na(i)
 
-    if (any(j)) {
-      stop("Source columns not found: ", paste(from[j], collapse=", "))
-    }
-    if (any(dup_from)) {
-      data[to[dup_from]] <- data[i[dup_from]]
-    }
-    names(data)[i[!dup_from]] <- to[!dup_from]
+  if (any(j)) {
+    stop("Source columns not found: ", paste(from[j], collapse=", "))
   }
+  if (any(dup_from)) {
+    data[to[dup_from]] <- data[i[dup_from]]
+  }
+  names(data)[i[!dup_from]] <- to[!dup_from]
   data
 }
--- tests/testthat/test-rename.R
+++ tests/testthat/test-rename.R
@@ -10,13 +10,21 @@ test_that("simple", {
   cmp <- d
   names(cmp)[[1]] <- "to1"
   expect_equal(rename(d, c("from1"="to1")), cmp)
+  expect_equal(rename(d, c("from1"="to1"), FALSE), cmp)
+  expect_equal(rename(d, c("to1"="from1")), cmp)
   expect_equal(rename(d, c("to1"="from1"), TRUE), cmp)
 })
 
 test_that("yaml", {
   d <- data.frame(from1=1:10, from2=runif(10))
   res1 <- rename(d, "rename_table.yml")
-  res2 <- rename(d, "rename_table_reverse.yml", TRUE)
+  res2 <- rename(d, "rename_table.yml", FALSE)
+  res3 <- rename(d, "rename_table_reverse.yml")
+  res4 <- rename(d, "rename_table_reverse.yml", TRUE)
+  expect_equal(res1, setNames(d, c("to1", "to2")))
+  expect_equal(res1, res2)
+  expect_equal(res1, res3)
+  expect_equal(res1, res4)
 })
 
 test_that("duplicates", {
