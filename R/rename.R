e8750dcabc832d708a20120574b9e0d7dafc471a
Rich FitzJohn <rich.fitzjohn@gmail.com>
2016-06-30 12:22:51 +0100

    Reverse direction of from/to mappings

--- R/rename.R
+++ R/rename.R
@@ -1,7 +1,7 @@
 ## There is _massive_ overlap here with dplyr, but we'll take a
 ## different approach (it's not designed for interactive work but for
 ## bulk processing).
-rename <- function(data, table) {
+rename <- function(data, table, reverse_direction=FALSE) {
   if (possible_filename(table) && file.exists(table)) {
     table <- yaml_read(table)
   }
@@ -9,23 +9,33 @@ rename <- function(data, table) {
     ## TODO: validation.
     table <- unlist(table)
   }
-  dup_to <- duplicated(names(table))
+
+  if (reverse_direction) {
+    from <- unname(table)
+    to <- names(table)
+  } else {
+    to <- unname(table)
+    from <- names(table)
+  }
+
+  dup_to <- duplicated(to)
+  dup_from <- duplicated(from)
+
   if (any(dup_to)) {
     stop("Duplicated destination columns: ",
-         paste(names(table)[dup_to], collapse=", "))
+         paste(to[dup_to], collapse=", "))
   }
-  dup_from <- duplicated(table)
-  if (length(table) > 0L) {
-    i <- match(table, names(data))
+  if (length(from) > 0L) {
+    i <- match(from, names(data))
     j <- is.na(i)
 
     if (any(j)) {
-      stop("Source columns not found: ", paste(names(data)[j], collapse=", "))
+      stop("Source columns not found: ", paste(from[j], collapse=", "))
     }
     if (any(dup_from)) {
-      data[names(table[dup_from])] <- data[i[dup_from]]
+      data[to[dup_from]] <- data[i[dup_from]]
     }
-    names(data)[i[!dup_from]] <- names(table[!dup_from])
+    names(data)[i[!dup_from]] <- to[!dup_from]
   }
   data
 }
--- tests/testthat/test-rename.R
+++ tests/testthat/test-rename.R
@@ -5,14 +5,25 @@ test_that("empty", {
   expect_identical(rename(d, character()), d)
 })
 
+test_that("simple", {
+  d <- data.frame(from1=1:10, from2=runif(10))
+  cmp <- d
+  names(cmp)[[1]] <- "to1"
+  expect_equal(rename(d, c("from1"="to1")), cmp)
+  expect_equal(rename(d, c("to1"="from1"), TRUE), cmp)
+})
+
 test_that("yaml", {
   d <- data.frame(from1=1:10, from2=runif(10))
-  rename(d, "rename_table.yml")
+  res1 <- rename(d, "rename_table.yml")
+  res2 <- rename(d, "rename_table_reverse.yml", TRUE)
 })
 
 test_that("duplicates", {
   d <- data.frame(from1=1:10, from2=runif(10))
-  d2 <- rename(d, c(to1="from1", to2="from1"))
+  d2 <- rename(d, c(from1="to1", from1="to2"))
+  d3 <- rename(d, c(to1="from1", to2="from1"), TRUE)
   expect_equal(names(d2), c("to1", "from2", "to2"))
   expect_identical(d2$to1, d2$to2)
+  expect_equal(d3, d2)
 })
